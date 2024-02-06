f416e5a65c52ef1bf08d143674940d39cb45ec6c
otaviovasc-inteli <otavio.vasconcelos@sou.inteli.edu.br>
2024-02-06 16:01:40 -0300

    finish

--- exercicioAula01.js
+++ exercicioAula01.js
@@ -1,5 +1,13 @@
-var nota1 = 7
-var nota2 = 3
+var nota1 = 7;
+var nota2 = 3;
 
 var mediaPraPassar = 6;
 
+if(media >= mediaPraPassar)
+{
+  console.log("Aprovado");
+}
+else
+{
+  console.log("Reprovado");
+}
