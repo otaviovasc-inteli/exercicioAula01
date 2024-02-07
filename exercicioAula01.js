1394a3624b9d8e1747620e1e06aa0b14f7e2513a
otaviovasc-inteli <159147379+otaviovasc-inteli@users.noreply.github.com>
2024-02-07 08:28:27 -0300

    Update exercicioAula01.js

--- exercicioAula01.js
+++ exercicioAula01.js
@@ -2,6 +2,10 @@ var nota1 = 7;
 var nota2 = 3;
 
 var mediaPraPassar = 6;
+var media = (nota1 + nota2) / 2;
+
+console.log("Nota p1: ", nota1, ". Nota p2:", nota2);
+console.log("Media para passar: ", mediaPraPassar);
 
 if(media >= mediaPraPassar)
 {
